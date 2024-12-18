1d9d14d4eb92fa582d9f201043c8dff985e1bb6b
6kumaran <kumaran61003@gmail.com>
2024-12-18 18:27:36 +0530

    Corrected Create page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -24,7 +24,7 @@ const CreatePage = () => {
     <Container>
       <Toaster position="bottom-center" reverseOrder={false} />
       <VStack spacing={8}>
-        <Heading as={'h1'} size={"2xl"} textAlign={"center"} mb={8}>Creat New Product</Heading>
+        <Heading as={'h1'} size={"2xl"} textAlign={"center"} m={8}>Creat New Product</Heading>
         <Box w={"full"} bg={useColorModeValue("white", "gray.900")} p={6} rounded={"lg"} shadow={"md"}>
           <VStack spacing={4}>
             <Input placeholder='Product Name' name='name' value={newProduct.name} onChange={(e)=>setNewProduct({...newProduct, name:e.target.value})}/>
